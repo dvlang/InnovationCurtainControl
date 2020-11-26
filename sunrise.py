c04588d93907efb76ad5439e6bcd8c252e5d24da
dvlang <daniel.v.lang@gmail.com>
2020-11-25 21:04:19 -0600

    cleaned up program, made the getsuntime() function so it can just be called and return the sunrise or sunset time

--- sunrise.py
+++ sunrise.py
@@ -3,48 +3,77 @@ import ephem
 import datetime
 import sys
 
-
-o=ephem.Observer()  
-o.lat='41.99'  
-o.long='-87.98'  
-s=ephem.Sun()  
-s.compute()  
-#print ephem.localtime(o.next_rising(s))  
-#print ephem.localtime(o.next_setting(s))  
-
-
+####################################################################################################
+##  
+##  function:    runmotor()
+##  inputs: diretion (open|close), rotations(int)
+##  return: 
+##  Description: this function will run the motor in the direction set for the rotations requested
+##
+####################################################################################################
+##// BEGIN runmotor//###
 def runmotor(direction,rotations):
     print("running motor in direction ", direction, " and for rotations= ", rotations)
+##// END runmotor//###
 
-now = datetime.datetime.now()
-sunrise = ephem.localtime(o.next_rising(s))  
-sunset=ephem.localtime(o.next_setting(s))  
+####################################################################################################
+##  
+##  function:    getsuntime()
+##  inputs: direction (sunset | sunrise)
+##  return: 
+##  Description: this function will run the motor in the direction set for the rotations requested
+##
+####################################################################################################
+##// BEGIN getsuntime//###
+def getsuntime(direction):
+    o=ephem.Observer()  
+    o.lat='41.99'  
+    o.long='-87.98'  
+    s=ephem.Sun()  
+    s.compute()  
+    #print ephem.localtime(o.next_rising(s))  
+    #print ephem.localtime(o.next_setting(s))  
 
-#print(sunrise)
-#print(sunset)
-#print(now)
+    sunrise = ephem.localtime(o.next_rising(s))  
+    sunset=ephem.localtime(o.next_setting(s))  
 
-#set up test times
-tsunrise= sunrise.replace(month=11, day=25) #make it today
-tsunset= sunset.replace(month=11, day=25) #make it today
-timetocheck = now.replace(month=11, day=25, hour=int(sys.argv[1]), minute=0, second=0, microsecond=0)
+    #print(sunrise)
+    #print(sunset)
+    #print(now)
 
-print(tsunrise)
-print(tsunset)
-print(timetocheck)
+    #set up test times
+    tsunrise= sunrise.replace(month=11, day=25) #make it today
+    tsunset= sunset.replace(month=11, day=25) #make it today
 
+
+    #print(tsunrise)
+    #print(tsunset)
+    #print(timetocheck)
+
+    if(direction=="sunrise"):
+        return tsunrise         #change to sunrise for not testcase
+    elif(direction=="sunset"):
+        return tsunset          #change to sunset for not testcase
+    else:
+        return null
+##// END getsuntime//###
+
+
+
+now = datetime.datetime.now()       
 today1am = now.replace(hour=1, minute=0, second=0, microsecond=0)
-
+timetocheck = now.replace(month=11, day=25, hour=int(sys.argv[1]), minute=0, second=0, microsecond=0)
+    
 openRunOnce = False
 closeRunOnce= False
 
-if(timetocheck>tsunrise):    #replace timetocheck with now for normal>
+if(timetocheck>getsuntime("sunrise")):    #replace timetocheck with now for not testcase>
     openRunOnce=True
     runmotor("open",4)
     position=1
     print("sunrise open")
     
-if(timetocheck>tsunset):
+if(timetocheck>getsuntime("sunset")):
     closeRunOnce=True
     runmotor("close",4)
     position=0
@@ -55,13 +84,3 @@ if(today1am==timetocheck.replace(minute=0, second=0, microsecond=0)):
     closeRunOnce= False
     print("reset for day")
     
-
-    
-
-
-#if(sunrisetime < timetocheck):
- #   print("<true")
-#if(sunrisetime == timetocheck):
- #   print("=true")
-#if(sunrisetime > timetocheck):
- #   print(">true")
